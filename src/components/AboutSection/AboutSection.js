7978ba8861391db6010544d2706504f636b88079
Parker Manning <parkerlmanning@hotmail.com>
2021-10-11 11:46:28 -0500

    about me card complete, project cards in progress

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -7,13 +7,16 @@ import {
 
 } from '@mui/material';
 import { Box } from '@mui/system';
+import ProfileCard from '../ProfileCard/ProfileCard';
+import ProjectCard from '../ProjectCard/ProjectCard';
 import boys from '../../images/boys.PNG';
+import pic from '../../images/portfolio pic.jpg';
 
 export default function AboutSection() {
 
     return (
         <Box sx={{
-                height: '92vh',
+                minHeight: '92vh',
                 position: 'relative',
                 backgroundColor: 'primary.main',
                 backgroundSize: 'cover',
@@ -21,24 +24,33 @@ export default function AboutSection() {
                 backgroundRepeat: 'no-repeat',
                 objectFit: 'cover',
                 paddingBottom: '5vh',
-            }}>
-            <Grid container flexDirection="column" padding={4} spacing={3} textAlign="left">
-                <Grid item xs={12}>
+        }}
+        >
+            <Grid container padding={2} spacing={1}>
+                <Grid item xs={8} lg={12}>
                     <Typography color="primary.contrastText" variant="h3">Hi,</Typography>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={8} lg={12}>
                     <Typography color="primary.contrastText" variant="h3">I'm Parker,</Typography>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={8} lg={3}>
                     <Typography color="secondary.main" variant="h3">Web Developer.</Typography>
                 </Grid>
-                <Grid item xs={12} lg={3} md={4}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={boys}
-                        alt="profile pic"
+                <Grid item lg={12}>
+                    <ProfileCard />
+                </Grid>
+                <Grid item lg={12} xs={12}>
+                    <Typography paddingTop={10} paddingBottom={6} align="left" color="secondary.main" variant="h3"> Projects </Typography>
+                </Grid>
+                <Grid item lg={6} align="center">
+                    <ProjectCard
+                        title="Social Media Site"
+                        subheader="Creator: Parker Manning"
+                        alt="Social Media Site Image"
                     />
+                </Grid>
+                <Grid item lg={6} align="center">
+                    <ProjectCard />
                 </Grid>
             </Grid>
         </Box>
